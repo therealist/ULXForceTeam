59abe54475a018fae4a99543469b559e9ab72666
therealist <schtherealist@hotmail.com>
2014-07-24 05:34:49 -0500

    Create forceteamspawn.lua

---
+++ lua/ulx/modules/sh/forceteamspawn.lua
@@ -0,0 +1,48 @@
+local ulx_team_table = {  
+"Hunter",
+"Prop", 
+"Spectator"
+}
+
+function ulx.forceteamspawn( calling_ply, target_plys, team, should_reset )
+
+	if not should_reset then
+	
+		local teamarg2
+
+		if team == "Hunter" then
+			teamarg2 = TEAM_HUNTERS
+		elseif team == "Prop" then
+			teamarg2 = TEAM_PROPS
+		elseif team == "Spectator" then
+			teamarg2 = TEAM_SPECTATOR
+		end
+		
+		for _, v in ipairs( target_plys ) do
+			v:Kill()
+			v:SetTeam( teamarg2 )
+			v:Spawn()
+		end
+
+		ulx.fancyLogAdmin( calling_ply, "#A set the team for #T to #s and spawned", target_plys, team )
+		
+	else
+	
+		for _, v in ipairs( target_plys ) do 
+			v:Kill()
+			v:SetTeam( TEAM_SPECTATOR )
+			v:Spawn()
+		end
+		
+		ulx.fancyLogAdmin( calling_ply, "#A reset the team for #T", target_plys )
+		
+	end
+	
+end
+local forceteamspawn = ulx.command( "Prop Hunt", "ulx forceteamspawn", ulx.forceteamspawn, "!fts" )
+forceteamspawn:addParam{ type=ULib.cmds.PlayersArg }
+forceteamspawn:addParam{ type=ULib.cmds.StringArg, hint="Select a Team", completes=ulx_team_table, ULib.cmds.restrictToCompletes }
+forceteamspawn:addParam{ type=ULib.cmds.BoolArg, invisible=true }
+forceteamspawn:defaultAccess( ULib.ACCESS_ADMIN )
+forceteamspawn:help( "Set a player's team." )
+forceteamspawn:setOpposite( "ulx resetteamspawn", { _, _, _, true }, "!resetteamspawn" )
